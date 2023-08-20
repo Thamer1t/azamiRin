46bda4287207235405dd6cb93055950f3784b7a3
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-20 20:33:51 +0300

    Update kandang.js

--- plugins/rpg/kandang.js
+++ plugins/rpg/kandang.js
@@ -3,28 +3,28 @@ import db from '../../lib/database.js'
 let handler = async (m, { conn, usedPrefix }) => {
 	let user = db.data.users[m.sender]
 	let gmbrt = 'https://telegra.ph/file/295a6d5105771875e1797.jpg'
-	let hsl = `*━━━━━ [ Kandang Buruan ] ━━━━━*
+	let hsl = `*━━━━━ [ الصيد ] ━━━━━*
 
-*🐂 = [ ${user.banteng} ] banteng*
-*🐅 = [ ${user.harimau} ] harimau*
-*🐘 = [ ${user.gajah} ] gajah*
-*🐐 = [ ${user.kambing} ] kambing*
-*🐼 = [ ${user.panda} ] panda*
-*🐊 = [ ${user.buaya} ] buaya*
-*🐃 = [ ${user.kerbau} ] kerbau*
-*🐮 = [ ${user.sapi} ] sapi*
-*🐒 = [ ${user.monyet} ] monyet*
-*🐗 = [ ${user.babihutan} ] babihutan*
-*🐖 = [ ${user.babi} ] babi*
-*🐓 = [ ${user.ayam} ] ayam*
+*🐂 = [ ${user.ثور} ] ثور*
+*🐅 = [ ${user.نمر} ] نمر*
+*🐘 = [ ${user.فيل} ] فيل*
+*🐐 = [ ${user.ماعز} ] ماعز*
+*🐼 = [ ${user.باندا} ] باندا*
+*🐊 = [ ${user.تمساح} ] تمساح*
+*🐃 = [ ${user.جاموس} ] جاموس*
+*🐮 = [ ${user.بقرة} ] بققرة*
+*🐒 = [ ${user.قرد} ] قرد*
+*🦎 = [ ${user.ضب} ] ضب*
+*🐖 = [ ${user.خنزير} ] خنزير*
+*🐓 = [ ${user.دجاجة} ] دجاجة*
 
 Gunakan *${usedPrefix}sell* untuk dijual atau *${usedPrefix}cook* untuk dijadikan bahan masakan.`
 
 	await conn.sendMsg(m.chat, { image: { url: gmbrt }, caption: hsl }, { quoted: m })
 }
 
-handler.menufun = ['kandang']
+handler.menufun = ['صيد']
 handler.tagsfun = ['rpg']
-handler.command = /^(kandang)$/i
+handler.command = /^(صيد)$/i
 
 export default handler
