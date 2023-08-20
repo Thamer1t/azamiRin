e1104be27b9530ad66be41a80c7a59d5415c11c4
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-20 15:25:38 +0300

    Update buah.js

--- plugins/rpg/buah.js
+++ plugins/rpg/buah.js
@@ -2,18 +2,18 @@ import db from '../../lib/database.js'
 
 let handler = async (m, { conn, usedPrefix, text }) => {
 	let user = db.data.users[m.sender]
-	let txt = `[ *GUDANG BUAH KAMU* ]\n\n`
-	txt += `🍌 ${user.pisang} Pisang\n`
-	txt += `🍇 ${user.anggur} Anggur\n`
-	txt += `🥭 ${user.mangga} Mangga\n`
-	txt += `🍊 ${user.jeruk} Jeruk\n`
-	txt += `🍎 ${user.apel} Apel\n\n`
-	txt += `Gunakan command *${usedPrefix}sell* untuk menjual.`
+	let txt = `[ *قائمة الفواكه* ]\n\n`
+	txt += `🍌 ${user.موز} موز\n`
+	txt += `🍇 ${user.عنب} عنب\n`
+	txt += `🥭 ${user.مانجو} مانجو\n`
+	txt += `🍊 ${user.برتقال} برتقال\n`
+	txt += `🍎 ${user.تفاح} تفاح\n\n`
+	txt += `استخدم *${usedPrefix}بيع* <اسم الفاكهة> عشان تبيع الفواكه`
 	m.reply(txt)
 }
 
-handler.menufun = ['buah']
+handler.menufun = ['فواكه']
 handler.tagsfun = ['rpg']
-handler.command = /^((list)?(buah|fruits?))$/i
+handler.command = /^((قائمة)?(الفواكه|fruits?))$/i
 
 export default handler
