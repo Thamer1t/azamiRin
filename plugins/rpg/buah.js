364415b0131dd26ff7af884134189c54e6b968d7
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-08-20 20:18:56 +0300

    Update buah.js

--- plugins/rpg/buah.js
+++ plugins/rpg/buah.js
@@ -14,6 +14,6 @@ let handler = async (m, { conn, usedPrefix, text }) => {
 
 handler.menufun = ['فواكه']
 handler.tagsfun = ['rpg']
-handler.command = /^الفواكه$/i
+handler.command = /^(الفواكه)$/i
 
 export default handler
