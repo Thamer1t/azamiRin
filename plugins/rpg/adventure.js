100a715107454114c0d2aadcedc1e2dc736c1bc3
Thamer1t <131318983+Thamer1t@users.noreply.github.com>
2023-10-08 09:59:20 +0300

    Update adventure.js

--- plugins/rpg/adventure.js
+++ plugins/rpg/adventure.js
@@ -1,18 +1,19 @@
-//const cooldown = 1000 // 1 detik
-//const cooldown = 60000 // 1 menit
-//const cooldown = 3600000 // 1 jam
-//const cooldown = 86400000 // 1 hari
-//const cooldown = 2592000000 // 1 bulan
+//const cooldown = 1000 // 1 ثانية
+//const cooldown = 60000 // 1 دقيقة
+//const cooldown = 3600000 // 1 ساعة
+//const cooldown = 86400000 // 1 يوم
+//const cooldown = 2592000000 // 1 شهر
 import db from '../../lib/database.js'
 import { ranNumb } from '../../lib/func.js'
 
 const cooldown = 900000
 
 let handler = async (m, { usedPrefix, command }) => {
-	let user = db.data.users\[m.sender\]
-let timers = (cooldown - (new Date() - user.lastadventure))
-if (user.صحة \< 80) return m.reply(`تحتاج إلى *❤️ صحة 80* على الأقل لـ ${command}!!\n\nاكتب *${usedPrefix}heal* لزيادة الصحة.\nأو اكتب *${usedPrefix}استخدام جرعة* لاستخدام الجرعة.`)
-if (new Date() - user.lastadventure \<= cooldown) return m.reply(`لقد قمت بالمغامرة بالفعل، يرجى الانتظار\n*🕐${timers.toTimeString()}*`)
+	let user = db.data.users[m.sender]
+	let timers = (cooldown - (new Date - user.lastadventure))
+	if (user.health < 80) return m.reply(`تحتاج على الأقل *❤️ 80 صحة* لـ ${command}!!\n\nاكتب *${usedPrefix}heal* لإضافة الصحة.\nأو *${usedPrefix}use potion* لاستخدام الجرعة.`)
+	if (new Date - user.lastadventure <= cooldown) return m.reply(`لقد قمت بالمغامرة بالفعل, الرجاء الانتظار\n*🕐${timers.toTimeString()}*`)
+
 	user.adventurecount += 1
 
 	const health = ranNumb(3, 6)
@@ -34,30 +35,30 @@ if (new Date() - user.lastadventure \<= cooldown) return m.reply(`لقد قمت 
 	user.حجر += rock
 	user.خشب += wood
 	user.خيط += string
-	if (user.adventurecount % 25  == 0) user.شائع  += common
-	if (user.adventurecount % 50  == 0) user.ذهب	+= gold
-	if (user.adventurecount % 150 == 0) user.زمرد += emerald
-	if (user.adventurecount % 400 == 0) user.الماس += diamond
+	if (user.adventurecount % 25  == 0) user.common  += common
+	if (user.adventurecount % 50  == 0) user.gold	+= gold
+	if (user.adventurecount % 150 == 0) user.emerald += emerald
+	if (user.adventurecount % 400 == 0) user.diamond += diamond
 
-	let txt = `[ *Selesai ${command}* ]\n\n`;
-txt += `*❤️ الصحة : -${health}*\nغامرت وحصلت :\n`;
-txt += `*💵 المال:* ${money}\n`;
-txt += `*✉️ الخبرة:* ${exp}\n`;
-txt += `*🗑 القمامة:* ${trash}\n`;
-txt += `*🪨 الصخور:* ${rock}\n`;
-txt += `*🪵 الخشب:* ${wood}\n`;
-txt += `*🕸️ الخيوط:* ${string}`;
-if (user.adventurecount % 25 == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*📦 شائع:* ${common}`;
-if (user.adventurecount % 50 == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*👑 ذهب:* ${gold}`;
-if (user.adventurecount % 150 == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*💚 زمرد:* ${emerald}`;
-if (user.adventurecount % 400 == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*💎 الماس:* ${diamond}`;
-m.reply(txt);
-user.lastadventure = new Date * 1
+	let txt = `[ *أنهيت ${command}* ]\n\n`
+	txt += `*❤️ الصحة : -${health}*\nالغنائم :\n`
+	txt += `*💵 المال :* ${money}\n`
+	txt += `*✉️ الخبرة :* ${exp}\n`
+	txt += `*🗑 القمامة :* ${trash}\n`
+	txt += `*🪨 الصخور :* ${rock}\n`
+	txt += `*🪵 الخشب :* ${wood}\n`
+	txt += `*🕸️ الخيوط :* ${string}`
+	if (user.adventurecount % 25  == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*📦 صندوق عادي :* ${common}`
+	if (user.adventurecount % 50  == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*👑 الذهب :* ${gold}`
+	if (user.adventurecount % 150 == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*💚 الزمرد :* ${emerald}`
+	if (user.adventurecount % 400 == 0) txt += `\n\nمكافأة المغامرة ${user.adventurecount} مرة\n*💎 الماس :* ${diamond}`
+	m.reply(txt)
+	user.lastadventure = new Date *1
 }
 
 handler.menufun = ['مغامرة', 'غامر', 'berpetualang', 'mulung']
 handler.tagsfun = ['rpg']
-handler.command = /^(مغامرة|(ber)?petualang(ang)?|غامر)$/i
+handler.command = /^(مغامرة|(ber)?petualang(ang)?|mulung)$/i
 
 handler.cooldown = cooldown
 
